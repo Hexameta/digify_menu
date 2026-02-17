3feae27d060807a11fbe651a5cee3bdbb37e7659
riyaskh123 <riyaskh123@gmail.com>
2026-02-17 14:53:49 +0400

    location track

--- src/Home.jsx
+++ src/Home.jsx
@@ -941,11 +941,11 @@ const DigifyLanding = () => {
   const [countryData, setCountryData] = useState(COUNTRYDATA.IN);
 
   useEffect(() => {
-    fetch("http://ip-api.com/json/")
+    fetch("https://ipapi.co/json/")
       .then((res) => res.json())
       .then((data) => {
-        console.log(data.countryCode); // "AE"
-        console.log(data.country); // "United Arab Emirates"
+        console.log(data.country); // "AE"
+        console.log(data.country_name); // "United Arab Emirates"
         setCountryData(COUNTRYDATA[data.country] || COUNTRYDATA.IN);
       });
   }, []);
